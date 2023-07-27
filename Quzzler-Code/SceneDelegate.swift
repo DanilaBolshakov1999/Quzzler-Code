def43adec6fe396a813cbdc0b75bf45a9861a68e
Danila Bolshakov <danila1999777@gmail.com>
2023-07-27 19:52:08 +0300

    initial commit

---
+++ Quzzler-Code/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  Quzzler-Code
+//
+//  Created by Danila Bolshakov  on 27.07.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Quzzler-Code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quzzler-Code
+//
+//  Created by IOS - Developer  on 27.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 36/255, green: 41/255, blue: 72/255, alpha: 1)
+    }
+}
+
